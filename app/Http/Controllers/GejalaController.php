c6dc28faa99762f57a24598d0901fe8a426e2102
pemula11 <andriaditya311@gmail.com>
2022-11-05 12:28:49 +0700

    fix bug

--- app/Http/Controllers/GejalaController.php
+++ app/Http/Controllers/GejalaController.php
@@ -2,17 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\gejala;
 use Illuminate\Http\Request;
 
 class GejalaController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        # code...
         return view('dashboard.gejala.index', [
             'tittle' => 'Login',
             'active' => 'login'
         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\gejala  $gejala
+     * @return \Illuminate\Http\Response
+     */
+    public function show(gejala $gejala)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\gejala  $gejala
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(gejala $gejala)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\gejala  $gejala
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, gejala $gejala)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\gejala  $gejala
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(gejala $gejala)
+    {
+        //
     }
 }
